c02af8487c808bfdf319688c7b292fe2e521b2c3
пк <@DESKTOP-70U3VC3>
2023-02-09 16:25:52 +0200

    Added Class Product, List<Product> products and adding it to ListBox Products

--- ShopLinqWPF/MainWindow.xaml.cs
+++ ShopLinqWPF/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -23,6 +24,41 @@ namespace ShopLinqWPF
         public MainWindow()
         {
             InitializeComponent();
+            foreach (var item in products)
+            {
+                ProductListBox.Items.Add(item);
+            }
+        }
+
+        List<Product> products = new List<Product>() { new Product("Apple", "Fruit", 20), new Product("Banana", "Fruit", 40),
+        new Product("Orange", "Fruit", 30), new Product("Strawberry", "Fruit", 400), new Product("Pinapple", "Fruit", 200),
+        new Product("Avocado", "Fruit", 260), new Product("Carrot", "Vegetables", 40), new Product("Onion", "Vegetables", 15),
+        new Product("Potato", "Vegetables", 20), new Product("Potato", "Pepper", 110), new Product("Cucumber", "Vegetables", 70),
+        new Product("Tomato", "Vegetables", 90), new Product("Milk", "Dairy", 40), new Product("Butter", "Dairy", 100),
+        new Product("Cheese", "Dairy", 170), new Product("Yoghurt", "Dairy", 60), new Product("Chicken", "Meat", 95),
+        new Product("Turkey", "Meat", 180),  new Product("Pork", "Meat", 200),  new Product("Beef", "Meat", 240),
+        new Product("Salmon", "Seafood", 600), new Product("Herring", "Seafood", 80), new Product("Salmon", "Seafood", 600),
+        new Product("Shrimp", "Seafood", 350), new Product("Rice", "Grains", 60), new Product("Buckwheat", "Grains", 35),
+        new Product("Beans", "Grains", 70), new Product("Cake", "Sweets", 300), new Product("Candies", "Sweets", 110),
+        new Product("Coockies", "Sweets", 80), new Product("Marshmello", "Sweets", 65), new Product("Chocolate", "Sweets", 150)}; 
+        
+        class Product
+        {
+            public Product(string name, string category, int price)
+            {
+                Name = name;
+                Category = category;
+                Price = price;
+            }
+
+            public string Name { get; set; }
+            public string Category { get; set; } 
+            public int Price { get; set; }
+
+            public override string ToString()
+            {
+                return $"{Name}, {Price}$. \t({Category})";
+            }
         }
     }
 }
